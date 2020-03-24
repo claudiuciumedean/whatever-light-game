c97bf4e3cde196c3970b00e265a73bd512699a9d
claudiuciumedean <claudiuciumedean@Claudius-MacBook-Pro.local>
2020-03-24 16:31:01 +0100

    working on the menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,14 @@ public class GameManager : Singleton<GameManager>
 {
     List<int> levelsIdx = new List<int> { 0, 1, 2, 3, 4, 5 };
     int currentLevel;
+    public GameObject pausePanel;
 
     public void startGame() {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("LevelOne");
     }
 
     public void pauseGame() {
-        Debug.Log("pausing the game");
+        pausePanel.SetActive(true);
     }
 
     public void goToNextLevel() {
