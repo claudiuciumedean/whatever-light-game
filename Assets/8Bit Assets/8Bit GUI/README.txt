88579dda44a8ed7f89357b56495133ec0661e70d
claudiuciumedean <claudiuciumedean@Claudius-MacBook-Pro.local>
2020-03-24 16:10:17 +0100

    working on the menu

---
+++ Assets/8Bit Assets/8Bit GUI/README.txt
@@ -0,0 +1,6 @@
+With this pack you can create your own UI 8bit style, like progressbars, checkboxes, radiobuttons and 39 exclusive buttons with 5 different colors.
+
+If you need more sprites, just request it to 
+http://www.csharpperls.it/blog/contatta-csharp-perls/ with email object "Request new sprite", and in the message, add the request and(optional but useful) a link of image example of the object that you want.
+
+Package asset store link: https://www.assetstore.unity3d.com/#!/content/64676
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    List<int> levelsIdx = new List<int> { 0, 1, 2, 3, 4, 5 };
+    int currentLevel;
+
+    public void startGame() {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void pauseGame() {
+        Debug.Log("pausing the game");
+    }
+
+    public void goToNextLevel() {
+        Debug.Log("Go to next level");
+    }
+}
